afbd9c2642b08750231d6df0195860a4065ac56d
Minchan ʕ • ᴥ • ʔ <118833626+Kkimzch@users.noreply.github.com>
2023-01-16 02:30:04 +0700

    v2

--- src/views/frontend/Home.vue
+++ src/views/frontend/Home.vue
@@ -6,10 +6,6 @@
                   
                   <h1 class="mb-4 text-5xl font-semibold leading-tight text-gray-800 capitalize">Smart Inventory system for your business growth </h1>
                   <p class="mb-4">ระบบคงคลังสินค้าที่จะมาช่วยเติมเต็มระบบของคุณให้เติบโตได้อย่างมั่นคง เริ่มต้นใช้งานระบบได้ฟรี</p>
-                   <p>URL: {{ url }}</p>
-                   <p>URLAPI: {{ urlapi }} </p>
-                   <p>TITLE: {{ title }} </p>
-                   <br>
                   <router-link to="/register" class="inline-block px-6 py-2 text-white bg-indigo-600 border-2 border-indigo-600 rounded-md hover:text-indigo-600 hover:bg-indigo-100">เริ่มต้นใช้งานฟรี</router-link>
                   
               </div>
